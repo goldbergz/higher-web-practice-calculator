f0d5653ab744793c183ab7254a7278e7dd8a2116
goldbergz <zolotogornaya22@mail.ru>
2026-01-12 11:22:17 +0300

    build: add dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
   server: {
     port: 3000,
+    open: true,
   },
   build: {
     outDir: 'dist',
